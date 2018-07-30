b31f55685c4da708b5108118d63c200fa9e38fb0
Didiet Noor <lynxluna@gmail.com>
2018-07-30 18:26:03 +0700

    Refactor out HTTP server

---
+++ flow/barito_producer_service_http.go
@@ -0,0 +1,149 @@
+package flow
+
+import (
+	"net/http"
+
+	"github.com/BaritoLog/go-boilerplate/errkit"
+	"github.com/BaritoLog/go-boilerplate/timekit"
+	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
+)
+
+type BaritoProducerServiceHTTP interface {
+	BaritoProducerService
+	ServeHTTP(rw http.ResponseWriter, req *http.Request)
+}
+
+type baritoProducerService struct {
+	factory       KafkaFactory
+	addr          string
+	topicSuffix   string
+	newEventTopic string
+
+	producer sarama.SyncProducer
+	admin    KafkaAdmin
+	server   *http.Server
+	limiter  RateLimiter
+}
+
+func newBaritoProducerServiceHTTP(factory KafkaFactory, addr string, maxTps int, topicSuffix string, newEventTopic string) BaritoProducerServiceHTTP {
+	return &baritoProducerService{
+		factory:       factory,
+		addr:          addr,
+		topicSuffix:   topicSuffix,
+		newEventTopic: newEventTopic,
+	}
+}
+
+func (s *baritoProducerService) Start() (err error) {
+
+	s.producer, err = s.factory.MakeSyncProducer()
+	if err != nil {
+		err = errkit.Concat(ErrMakeSyncProducer, err)
+		return
+	}
+
+	s.admin, err = s.factory.MakeKafkaAdmin()
+	if err != nil {
+		err = errkit.Concat(ErrMakeKafkaAdmin, err)
+		return
+	}
+
+	s.limiter = NewRateLimiter(timekit.Duration("1s"))
+	s.limiter.Start()
+
+	server := s.initHttpServer()
+	return server.ListenAndServe()
+}
+
+func (s *baritoProducerService) initHttpServer() (server *http.Server) {
+	server = &http.Server{
+		Addr:    s.addr,
+		Handler: s,
+	}
+
+	s.server = server
+	return
+}
+
+func (a *baritoProducerService) Close() {
+	if a.server != nil {
+		a.server.Close()
+	}
+
+	if a.limiter != nil {
+		a.limiter.Stop()
+	}
+
+	if a.admin != nil {
+		a.admin.Close()
+	}
+
+	if a.producer != nil {
+		a.producer.Close()
+	}
+
+}
+
+func (s *baritoProducerService) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	timber, err := ConvertRequestToTimber(req)
+	if err != nil {
+		onBadRequest(rw, err)
+		return
+	}
+
+	// add suffix
+	topic := timber.Context().KafkaTopic + s.topicSuffix
+
+	maxTokenIfNotExist := timber.Context().AppMaxTPS
+	if s.limiter.IsHitLimit(topic, maxTokenIfNotExist) {
+		onLimitExceeded(rw)
+		return
+	}
+
+	var newTopicCreated bool
+
+	if !s.admin.Exist(topic) {
+		numPartitions := timber.Context().KafkaPartition
+		replicationFactor := timber.Context().KafkaReplicationFactor
+
+		log.Infof("%s is not exist. Creating topic with partition:%v replication_factor:%v", topic, numPartitions, replicationFactor)
+
+		err = s.admin.CreateTopic(topic, numPartitions, replicationFactor)
+		if err != nil {
+			onCreateTopicError(rw, err)
+			return
+		}
+
+		s.admin.AddTopic(topic)
+		s.sendCreateTopicEvents(topic)
+		newTopicCreated = true
+	}
+
+	err = s.sendLogs(topic, timber)
+	if err != nil {
+		onStoreError(rw, err)
+		return
+	}
+
+	onSuccess(rw, ProduceResult{
+		Topic:      topic,
+		IsNewTopic: newTopicCreated,
+	})
+}
+
+func (s *baritoProducerService) sendLogs(topic string, timber Timber) (err error) {
+	message := ConvertTimberToKafkaMessage(timber, topic)
+	_, _, err = s.producer.SendMessage(message)
+	return
+}
+
+func (s *baritoProducerService) sendCreateTopicEvents(topic string) (err error) {
+	message := &sarama.ProducerMessage{
+		Topic: s.newEventTopic,
+		Value: sarama.ByteEncoder(topic),
+	}
+	_, _, err = s.producer.SendMessage(message)
+	return
+}
---
+++ flow/barito_producer_service_http_test.go
@@ -0,0 +1,155 @@
+package flow
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/BaritoLog/barito-flow/mock"
+	. "github.com/BaritoLog/go-boilerplate/testkit"
+	"github.com/BaritoLog/go-boilerplate/timekit"
+	"github.com/golang/mock/gomock"
+)
+
+func TestBaritoProducerService_ServeHTTP_OnLimitExceed(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	limiter := NewDummyRateLimiter()
+	limiter.Expect_IsHitLimit_AlwaysTrue()
+
+	srv := &baritoProducerService{
+		limiter: limiter,
+	}
+
+	req, _ := http.NewRequest("POST", "/", bytes.NewReader(sampleRawTimber()))
+	resp := RecordResponse(srv.ServeHTTP, req)
+
+	FatalIfWrongResponseStatus(t, resp, 509)
+}
+
+func TestBaritoProducerService_ServeHTTP_OnBadRequest(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	producer := mock.NewMockSyncProducer(ctrl)
+	producer.EXPECT().Close()
+
+	admin := mock.NewMockKafkaAdmin(ctrl)
+	admin.EXPECT().Close().AnyTimes()
+
+	agent := &baritoProducerService{
+		producer:    producer,
+		topicSuffix: "_logs",
+		admin:       admin,
+	}
+	defer agent.Close()
+
+	req, _ := http.NewRequest("POST", "/", strings.NewReader(`invalid-body`))
+	resp := RecordResponse(agent.ServeHTTP, req)
+
+	FatalIfWrongResponseStatus(t, resp, http.StatusBadRequest)
+}
+
+func TestBaritoProducerService_ServeHTTP_OnStoreError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	admin := mock.NewMockKafkaAdmin(ctrl)
+	admin.EXPECT().Exist(gomock.Any()).Return(true)
+
+	producer := mock.NewMockSyncProducer(ctrl)
+	producer.EXPECT().SendMessage(gomock.Any()).
+		Return(int32(0), int64(0), fmt.Errorf("some-error"))
+
+	limiter := NewDummyRateLimiter()
+
+	agent := &baritoProducerService{
+		producer:    producer,
+		topicSuffix: "_logs",
+		admin:       admin,
+		limiter:     limiter,
+	}
+
+	req, _ := http.NewRequest("POST", "/", bytes.NewReader(sampleRawTimber()))
+	resp := RecordResponse(agent.ServeHTTP, req)
+
+	FatalIfWrongResponseStatus(t, resp, http.StatusBadGateway)
+}
+
+func TestBaritoProducerService_ServeHTTP_OnCreateTopicError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	admin := mock.NewMockKafkaAdmin(ctrl)
+	admin.EXPECT().Exist(gomock.Any()).Return(false)
+	admin.EXPECT().CreateTopic(gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(fmt.Errorf("create-topic-error"))
+
+	producer := mock.NewMockSyncProducer(ctrl)
+
+	limiter := NewDummyRateLimiter()
+
+	agent := &baritoProducerService{
+		producer:    producer,
+		topicSuffix: "_logs",
+		admin:       admin,
+		limiter:     limiter,
+	}
+
+	req, _ := http.NewRequest("POST", "/", bytes.NewReader(sampleRawTimber()))
+	resp := RecordResponse(agent.ServeHTTP, req)
+
+	FatalIfWrongResponseStatus(t, resp, http.StatusServiceUnavailable)
+}
+
+func TestBaritoProducerService_ServeHTTP_OnSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	admin := mock.NewMockKafkaAdmin(ctrl)
+	admin.EXPECT().Exist(gomock.Any()).Return(false)
+	admin.EXPECT().CreateTopic(gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(nil)
+	admin.EXPECT().AddTopic(gomock.Any())
+
+	producer := mock.NewMockSyncProducer(ctrl)
+	producer.EXPECT().SendMessage(gomock.Any()).AnyTimes()
+
+	limiter := NewDummyRateLimiter()
+
+	agent := &baritoProducerService{
+		producer:    producer,
+		topicSuffix: "_logs",
+		admin:       admin,
+		limiter:     limiter,
+	}
+
+	req, _ := http.NewRequest("POST", "/", bytes.NewReader(sampleRawTimber()))
+	resp := RecordResponse(agent.ServeHTTP, req)
+
+	FatalIfWrongResponseStatus(t, resp, http.StatusOK)
+
+	var result ProduceResult
+	b, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(b, &result)
+
+	FatalIf(t, result.Topic != "some_topic_logs", "wrong result.Topic")
+	FatalIf(t, result.IsNewTopic != true, "wrong result.IsNewTopic")
+}
+
+func TestBaritoProducerService_Start_ErrorMakeSyncProducer(t *testing.T) {
+	factory := NewDummyKafkaFactory()
+	factory.Expect_MakeSyncProducerFunc_AlwaysError("some-error")
+
+	service := NewBaritoProducerService(factory, "addr", 1, "_logs", "new_topic_events")
+	err := service.Start()
+
+	FatalIfWrongError(t, err, "Make sync producer failed: some-error")
+}
